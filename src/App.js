e299c22a314716f8dbbec5e02735374f96361b8b
rvhora <rupeshvhora1988@gmail.com>
2022-07-28 09:44:37 +1000

    new changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload, finally deployed woohoo.. well done Rupesh - new!!!
+          Edit <code>src/App.js</code> and save to reload, finally deployed woohoo.. well done Rupesh - new--1!!!
         </p>
         <a
           className="App-link"
